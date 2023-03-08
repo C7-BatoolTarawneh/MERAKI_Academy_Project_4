d3c37d3aae60b0398cf7d40747b72fe9287cd239
BatoolTarawneh <batoolatarawneh@gmail.com>
2023-03-08 20:02:25 +0300

    Users/roles/tweets/replies DB/backend

--- backend/controllers/retweetController.js
+++ backend/controllers/retweetController.js
@@ -2,35 +2,38 @@ const tweetsModel = require("../models/tweets");
 const userModel = require("../models/users");
 const retweetModel = require("../models/retweet");
 
-
 const makeRetweet = async (req, res) => {
-    try {
-        const tweet = await tweetsModel.findById(req.params.id);    
-        if (!tweet){
-           return  res.status(404).json({message:"Tweet not found"})
-        }
-        //check if the tweet has been retweeted already
-        const existingRetweet = await retweetModel.findOne({tweet:tweet._id,retweeter:req.user._id})
-        if(existingRetweet)
-        {
-            return res.status(404).json({message:"you have already retweeted this tweet"})
-        }
-
-        const retweet = new retweetModel({
-            tweet : tweet._id,
-            retweeter: req.user._id
-        })
-        await retweet.save()
-        //update tweet
-        await tweet.save()
+  const retweeterId = req.token.userId;
+  const  tweetId  = req.params.id
+  try {
+    const tweet = await tweetsModel.findById(tweetId);
+    if (!tweet) {
+        console.log(tweetId)
+      return res
+        .status(404)
+        .json({ success: false, message: "Tweet not found" });
     }
-    catch(error){
-        res.status(500).json({
-            success: false,
-            message: `Server Error`,
-            error: error.message,
-          });
-        }
-    }
+   
+    const retweet = new retweetModel({
+      tweet: tweetId,
+      retweeter: retweeterId,
+    });
+    //update tweet
+    await retweet.save();
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "retweet created successfully",
+        retweet: retweet,
+      });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Server Error`,
+      error: error.message,
+    });
+  }
+};
 
-        module.exports = {makeRetweet}
+module.exports = { makeRetweet };

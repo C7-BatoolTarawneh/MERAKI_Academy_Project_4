18fe2daf70fd380974a4dadc583f9bee998d172d
BatoolTarawneh <batoolatarawneh@gmail.com>
2023-03-11 22:55:46 +0300

    working on cloudenary

--- frontend/src/components/TweetCard/index.js
+++ frontend/src/components/TweetCard/index.js
@@ -15,39 +15,51 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import { Modal, Box, TextField, Button } from "@mui/material";
 
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { UserContext } from "../../App";
-
 import "./style.css";
 
 const TweetCard = () => {
   const [tweets, setTweets] = useState([]);
-  const { isLoggedIn, token ,user} = useContext(UserContext);
+  const { isLoggedIn, token, user } = useContext(UserContext);
   const [userId, setUserId] = useState("");
-  //anchorEl is used to control whether the Menu component is displayed or hidden.
+//   const{tweetId} = useParams()
+const [updatedTweet, setUpdatedTweet] = useState({description:"",image:""})
+const [tweetId, setTweetId] = useState("");
+const [isUpdating,setIsUpdating] = useState("")
+ //states for image anddescription
+//  const [tweetDescription, setTweetDescription] = useState("");
+//  const [tweetImage, setTweetImage] = useState(null);
+
+    //anchorEl is used to control whether the Menu component is displayed or hidden.
   const [anchorEl, setAnchorEl] = useState(null);
   const [selectedTweet, setSelectedTweet] = useState(null);
-
-
+  //to monitore the modal state is shown or hidden
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const handleMenuClose = () => {
     setAnchorEl(null);
   };
-  
-//   const handleMenuClick = (event, tweet) => {
-//     setSelectedTweet(tweet);
-//     setAnchorEl(event.currentTarget);
-//   };
 
   const handleMenuClick = (event, tweet) => {
     setSelectedTweet(tweet);
     setAnchorEl(event.currentTarget);
   };
 
+  //open the modal when the EditIcon is clicked
+  
+  // if modal close
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+
   const getAllTweets = async () => {
+    
     try {
       const response = await axios.get("http://localhost:5000/tweets", {
         headers: {
@@ -57,39 +69,11 @@ const TweetCard = () => {
       console.log("response.data: ", response.data);
       setTweets(response.data.tweets);
       setUserId(response.data.userId);
-
     } catch (err) {
       console.log(err);
     }
   };
 
-
-  /*const handleDeleteTweet = async (tweetId) => {
-    try {
-        const response = await axios.delete(
-          `http://localhost:5000/tweets/delete/${tweetId}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-      if (response.data.success) {
-        const deletedItem = tweets.filter((t) => {
-          return t._id !== tweetId;
-          
-        });
-        console.log(deletedItem);
-        setTweets(deletedItem);
-        setSelectedTweet(null);
-        setAnchorEl(null);
-      }
-
-    } catch (error) {
-      console.log(error);
-    }
-  };*/
-  
   const handleDeleteTweet = async () => {
     try {
       const response = await axios.delete(
@@ -111,15 +95,68 @@ const TweetCard = () => {
       console.log(error);
     }
   };
+
+ 
   
+//handle edit
+const handleEditTweetSubmit = async (e) => {
+    
+    e.preventDefault();
+    console.log(tweetId);
+    console.log(tweetId);
+    console.log(e)
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/tweets/update/${tweetId}`,
+        {
+          description: updatedTweet.description,
+          image: updatedTweet.image,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.data.success) {
+        const updatedTweets = tweets.map((tweet) =>{if(tweet._id === tweetId)
+        {
+            return {...tweet,description: updatedTweet.description,image: updatedTweet.image}
+        }
+    return tweet
+})
+          
+       
+        setTweets(updatedTweets);
+        setIsUpdating(false)
+        setUpdatedTweet({description:"",image:""})
+        setAnchorEl(null);
+        setIsModalOpen(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   
-  
+  const handleEditTweetClick = (tweet) => {
+    // setTweetDescription(tweet.description);
+    // console.log(tweet.description)
+    // setTweetImage(tweet.image);
+    // setSelectedTweet(tweet);
+    setIsUpdating(true)
+    setUpdatedTweet({description:tweet.description,image:tweet.image})
+    setIsModalOpen(true);
+    setTweetId(tweet._id);
+    console.log(tweet._id)
+  };
+
   useEffect(() => {
     getAllTweets();
   }, []);
 
   const renderTweets = () => {
     return tweets.map((tweet) => (
+       
       <div className="card-pos" key={tweet._id}>
         <Card sx={{ maxWidth: 600 }}>
           <CardHeader
@@ -132,7 +169,6 @@ const TweetCard = () => {
               </Avatar>
             }
             action={
-            
               isLoggedIn &&
               tweet.writer._id === userId && (
                 <IconButton
@@ -169,44 +205,88 @@ const TweetCard = () => {
             </IconButton>
           </CardActions>
         </Card>
-       
+
         <Menu
           id="simple-menu"
           anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl) && selectedTweet?._id === tweet._id}
-        onClose={handleMenuClose}
-          
+          keepMounted
+          open={Boolean(anchorEl) && selectedTweet?._id === tweet._id}
+          onClose={handleMenuClose}
         >
           {tweet?.writer._id === userId && (
             <div>
-              <MenuItem >
-              
-                <ListItemIcon  >
-                  <DeleteIcon  fontSize="small" onClick={()=>handleDeleteTweet(tweet._id)} />
+              <MenuItem>
+                <ListItemIcon>
+                  <DeleteIcon
+                    fontSize="small"
+                    onClick={() => handleDeleteTweet(tweet._id)}
+                  />
                 </ListItemIcon>
                 <Typography variant="inherit" noWrap>
                   Delete Tweet
                 </Typography>
               </MenuItem>
-                <MenuItem>
-                  <ListItemIcon>
-                    <EditIcon fontSize="small" />
-                  </ListItemIcon>
-                  <Typography variant="inherit" noWrap>
-                    Update Tweet
-                  </Typography>
-                </MenuItem>
-             
+              <MenuItem>
+                <ListItemIcon>
+                  <EditIcon fontSize="small" onClick={()=>handleEditTweetClick(tweet)} />
+                </ListItemIcon>
+                <Typography variant="inherit" noWrap>
+                  Update Tweet
+                </Typography>
+              </MenuItem>
             </div>
           )}
         </Menu>
+        <Modal open={isModalOpen} onClose={handleModalClose}>
+  <Box
+    sx={{
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: 400,
+      bgcolor: "background.paper",
+      boxShadow: 24,
+      p: 4,
+    }}
+  >
+    <form onSubmit= {(E)=>{handleEditTweetSubmit(E)}}>
+      <TextField
+        label="Description"
+        fullWidth
+        multiline
+        rows={4}
+        value={updatedTweet.description}
+        onChange={(event) => setUpdatedTweet({...updatedTweet,description:event.target.value})}
+      />
+      <input
+        accept="image/*"
+        id="tweet-image-upload"
+        type="file"
+        onChange={(event) => setUpdatedTweet({...updatedTweet,image:event.target.value})}
+      />
+      {/* <label htmlFor="tweet-image-upload">
+        <Button variant="contained" component="span">
+          Upload Image
+        </Button>
+      </label> */}
+
+      <Button variant="contained" type="submit" >
+        Save Changes
+      </Button>
+    </form>
+  </Box>
+</Modal>
+
       </div>
       //   </div>
     ));
+    
+    
   };
 
   return <div>{renderTweets()}</div>;
+ 
 };
 
 export default TweetCard;
